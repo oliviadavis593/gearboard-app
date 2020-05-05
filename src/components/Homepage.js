a22d647eabd3653b0e4cf3136ac21fef45f4d059
Olivia Davis <olivia.davis593@gmail.com>
2020-05-04 21:48:12 -0500

    static client

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../styles/Homepage.css'
 
 class Homepage extends Component {
     render() {
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import '../styles/Landing.css'
+import '../styles/Landing.css';
+import {  Link } from 'react-router-dom';
 
 
 
@@ -10,16 +11,8 @@ function Landing() {
                     <h2>Keep tabs on your gear</h2>
                     <h3>Use GearBoard to track all of your instruments</h3>
                 <div className='btns'>
-                    <button
-                    className='login__button'
-                    >
-                        Login
-                    </button>
-                    <button
-                    className='registration__button'
-                    >
-                        Registration
-                    </button>
+                    <Link to='/login' className='login__button'>Login</Link>
+                    <Link to='/registration' className='registration__button'>Registration</Link>
                 </div>
             </div>
             <div className='landing-about__icons'>
@@ -33,6 +26,25 @@ function Landing() {
 
                 </div>
             </div>
+
+            <footer className='footer'>
+                <div className='footer__brand-container'>
+                    <p className='footer__author-text'>
+                        Created by <a href='https://oliviadavis593.github.io/portfolio/' target='_blank' rel='Portfolio'>Olivia Davis</a>
+                    </p>
+                </div>
+                <div className='footer__social-media'>
+                    <a href='https://github.com/oliviadavis593' target='_blank' rel='GitHub'>GitHub</a>
+                    <a href='https://www.linkedin.com/in/olivia-davis-ab1848105/' target='_blank' rel='LinkedIn'>LinkedIn</a>
+                </div>
+                <div className='copyright-text'>
+                    <p>
+                        Copyright &copy; 2020
+                    <br />
+                        All Rights Reserved
+                    </p>
+                </div>
+            </footer>
         </div>
     )
 }
