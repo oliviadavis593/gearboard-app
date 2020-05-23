5f05696380af8d68429ab37864f1e2004e63bd81
Olivia Davis <olivia.davis593@gmail.com>
2020-05-23 13:33:19 -0500

    Link removed

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
 import LandingHeader from '../../views/LandingHeader'
 import AuthApiService from '../../services/auth-api-service'
 import '../../styles/Registration.css'
