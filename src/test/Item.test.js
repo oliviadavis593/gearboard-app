a4ea3b27417781870a4e21597618e999087e5bd4
Olivia Davis <olivia.davis593@gmail.com>
2020-05-23 21:29:39 -0500

    snapshot tests passing

--- src/test/Item.test.js
+++ src/test/Item.test.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
 import Item from '../components/Item/Item'
 
 
-describe('Footer component', () => {
+describe.only('Item component', () => {
     it(`renders without crashing`, () => {
         const div = document.createElement('div')
         ReactDOM.render(<Item gear_name='whatever' id={2}/>, div)
         ReactDOM.unmountComponentAtNode(div)
+    })
+
+    it('renders the UI as expected', () => {
+        const tree = renderer
+            .create(<Item gear_name='test gear_name' id={1}/>)
+            .toJSON();
+        expect(tree).toMatchSnapshot();
     })
 })
