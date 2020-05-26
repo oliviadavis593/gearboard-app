3eae66b3e1657ec7b56468b695d23f26e89a12f1
Olivia Davis <olivia.davis593@gmail.com>
2020-05-25 21:04:22 -0500

    landing info icons

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -3,6 +3,9 @@ import '../styles/Landing.css';
 import { Link } from 'react-router-dom';
 import LandingHeader from './LandingHeader';
 import Footer from '../components/Footer/Footer'
+import Star from '../img/star.png'
+import List from '../img/list.png'
+import Check from '../img/check.png'
 
 
 function Landing() {
@@ -25,13 +28,16 @@ function Landing() {
             </div>
             <div className='landing-about__icons'>
                 <div className='rating__icon'>
-                    Card Info #1
+                    <img src={Star} alt='star' className='gb-star__landing'/>
+                    <p>Rate your gear</p>
                 </div>
-                <div className='track__icon'>
-                    Card Info #2
+                <div className='track__icon'> 
+                    <img src={Check} alt='check' className='gb-check__landing' />
+                    <p>Add your gear</p>
                 </div>
                 <div className='comment__icon'>
-                    Card Info #3
+                <img src={List} alt='list' className='gb-list__landing' />
+                    <p>List all quality features</p>
                 </div>
             </div>
             <Footer />
